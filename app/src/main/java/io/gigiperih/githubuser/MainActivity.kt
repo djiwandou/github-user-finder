6e39cb9a42ad60d5d58cc4b7a9cef82c61b43793
gilang <gilangabdulaziz@live.com>
2021-04-03 00:03:06 +0700

    rf main act

--- app/src/main/java/io/gigiperih/githubuser/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package io.gigiperih.githubuser
-
-import androidx.appcompat.app.AppCompatActivity
-
-class MainActivity : AppCompatActivity(R.layout.activity_main) {
-}
---
+++ app/src/main/java/io/gigiperih/githubuser/users/UsersActivity.kt
@@ -0,0 +1,7 @@
+package io.gigiperih.githubuser.users
+
+import androidx.appcompat.app.AppCompatActivity
+import io.gigiperih.githubuser.R
+
+class UsersActivity : AppCompatActivity(R.layout.activity_users) {
+}
