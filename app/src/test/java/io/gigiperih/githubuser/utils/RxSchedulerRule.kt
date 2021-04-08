48b1f422c5a6abe4d9e9e07630c976a596611b7b
gilang <gilangabdulaziz@live.com>
2021-04-08 22:18:06 +0700

    add unit test in presentation

---
+++ app/src/test/java/io/gigiperih/githubuser/utils/RxSchedulerRule.kt
@@ -0,0 +1,30 @@
+package io.gigiperih.githubuser.utils
+
+import io.reactivex.rxjava3.android.plugins.RxAndroidPlugins
+import io.reactivex.rxjava3.plugins.RxJavaPlugins
+import io.reactivex.rxjava3.schedulers.Schedulers
+import org.junit.rules.TestRule
+import org.junit.runner.Description
+import org.junit.runners.model.Statement
+
+class RxSchedulerRule : TestRule {
+
+    override fun apply(base: Statement, description: Description) =
+        object : Statement() {
+            override fun evaluate() {
+                RxAndroidPlugins.reset()
+                RxAndroidPlugins.setInitMainThreadSchedulerHandler { SCHEDULER_INSTANCE }
+
+                RxJavaPlugins.reset()
+                RxJavaPlugins.setIoSchedulerHandler { SCHEDULER_INSTANCE }
+                RxJavaPlugins.setNewThreadSchedulerHandler { SCHEDULER_INSTANCE }
+                RxJavaPlugins.setComputationSchedulerHandler { SCHEDULER_INSTANCE }
+
+                base.evaluate()
+            }
+        }
+
+    companion object {
+        private val SCHEDULER_INSTANCE = Schedulers.trampoline()
+    }
+}
